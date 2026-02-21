0fe1be79460307e114878ed65738553df6409363
wistrach <2024302111189@whu.edu.cn>
2026-02-21 17:07:06 +0800

    规范化main

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine
+from routers.templates import router as templates_router
 
 # --- 核心动作：创建数据库表 ---
 # 这一步非常关键！它会根据 models.py 自动生成 sql_app.db
@@ -29,5 +30,4 @@ def read_root():
 
 
 # 以后大家写的路由(Router)会在这里引入
-from routers.templates import router as templates_router
 app.include_router(templates_router)
